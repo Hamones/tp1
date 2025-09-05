46a562c978a833c09fadc73836ba8798c686983d
Hamones <63001546+Hamones@users.noreply.github.com>
2025-09-05 15:15:23 -0300

    Update e5.c

--- e5.c
+++ e5.c
@@ -1,2 +1,13 @@
 listar informações adicionais que podem ser alteradas a qualquer momento.
 Testar todas as funcionalidades para as aulas dentro do ambiente notion.
+#include <stdlib.h>
+  int *vet;
+  int tam;
+
+int main ()
+{
+  vet = malloc(tam*sizeof(int));
+  
+  return 0;
+    
+}
